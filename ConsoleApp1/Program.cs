d29b3c202ce78e68d8c67b3898946da0ca7fd79a
Tommy <tommy-sc@live.no>
2022-05-04 10:54:19 +0200

    Asd

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,111 @@
+﻿namespace consoleApp1
+{
+    class Program
+    {
+        // VARIABEL OPPGAVE 1: FERDIG
+        //Lag 8 variabler og til-ordne dem en verdi.
+        //Det skal være en variabel per hver type: int, long, float, decimal, double, string, char og bool.
+
+        //INT tall = 1; //Et Integer (Hel tall)
+        //LONG nummer = 0L; //64-bit signed integer -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+        //FLOAT floatNummer = 1.01f; // "f" Står for Float
+        //DECIMAL decimalNummer = 1.2m; // Decimal nummer
+        //DOUBLE doubleNummer = 114511.555d; // 64-bit double-precision floating point type
+        //STRING strBokstaver = "Wallah brur"; // Doble snutter i strings
+        //CHAR chrEnBokstav = 'A'; // Husk single snutter!!
+        //BOOL santUsant = false; // True or False
+
+        //double x;
+        //double y;
+        //double result;
+
+        //Console.WriteLine("Enter in number 1: ");
+        //    x = Convert.ToDouble(Console.ReadLine());
+
+        //    Console.WriteLine("Enter in number 2: ");
+        //    y = Convert.ToDouble(Console.ReadLine());
+
+        //    result = Multiply(x, y);
+
+        //Console.WriteLine(result);
+
+        //    Console.ReadKey();
+
+        //----------------------------------------------------------------------------------------------------------
+
+        //VARIABEL OPPGAVE 2: FERDIG
+        // Lag en konsoll-applikasjon som lager 3 variabler.De kan hete a, b og sum.
+
+        //a skal være en int
+        //b skal være en decimal. 
+        //Verdiene variablene skal ha er:
+
+
+        //a = 5
+        //b = 3.0
+        //sum er a + b.
+        //Hva slags type variabel må sum være?
+
+        //static void Main(string[] args)
+        //{
+        //    // return = returns data back to the place where the method is invoked
+        //    int a = 5;
+        //    decimal b = 3.0m;
+        //    double sum;
+        //    sum = Multiply(a, b);
+        //    Console.WriteLine(sum);
+        //}
+        //static double Multiply(int a, decimal b)
+        //{
+        //    double sum = (double)(a + b);
+        //    return sum;
+        //}
+
+        // --------------------------------------------------------------------------------------------------------
+
+        //OPPGAVER OM RETURVERDIER
+        // Oppgave 1: Lag en metode som tar inn og returnerer summen av to tall
+        // Oppgave 2: Lag en metode som returner "Denne metoden returnerer ingenting!" Why though?
+
+        //        static void Main(string[] args)
+        //        {
+        //            // return = returns data back to the place where the method is invoked
+        //            int a = 5;
+        //            decimal b = 3.0m;
+        //            double sum;
+        //            sum = Multiply(a, b);
+        //            Console.WriteLine(sum);
+        //            Console.WriteLine("Denne metoden returnerer ingenting!");
+        //        }
+        //        static double Multiply(int a, decimal b)
+        //        {
+        //            double sum = (double)(a + b);
+        //            return sum;
+        //        }
+        //    }
+        //}
+
+        // ----------------------------------------------------------------------------------------------------------
+        // OPPGAVER OM WRITELINE OG READLINE:
+        // Oppgave 1:
+        // Lag en konsoll-app som skriver til brukeren "Hei, hva heter du?",
+        // deretter leser inn input fra bruker og til slutt printer ut "Velkommen + det brukeren skrev"
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hei, hva heter du?");
+            String name = Console.ReadLine();
+
+            Console.WriteLine("Hvor gammel er du?");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Velkommen " + name + "!");
+            Console.WriteLine("Du er " + age + " år gammel.");
+
+            Console.ReadKey();
+            
+        }
+      
+    }
+}
---
+++ freeCodeCampTutorial/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+
+namespace freeCodeCampTutorial // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // METHODS
+            SayWallah("Brur", 33);
+            SayWallah("Bruh", 45);
+            SayWallah("Brun", 50);  // Fungerer som en funksjon
+
+            Console.ReadLine();
+        }
+
+        static void SayWallah(string name, int age)
+        {
+            Console.WriteLine("Wallah " + name + ", du er " + age + " år gammel.");
+        }
+    }
+}
+
+
+
+// VARIABLES
+
+//string characterName = "John";
+//int characterAge = 35;
+
+//Console.WriteLine("There once was a man named " + characterName);
+//Console.WriteLine("He was " + characterAge + " years old");
+//Console.WriteLine("He really liked the named " + characterName);
+//Console.WriteLine("But didn't like being " + characterAge);
+
+//Console.ReadLine();
+
+
+// DATA TYPES
+
+//string text = "Wallah Brur";
+//char letter = 'A';
+//int number = 30;
+//double gpa = 3.3;   // float, double, decimal //Mer og mer nøye fra venstre til høyre
+//bool isDank = true;
+
+//Console.WriteLine(30);
+
+//Console.ReadLine();
+
+
+// STRING METHODS
+
+//string phrase = "Wallah Brur";
+
+
+//Console.WriteLine(phrase.Substring(7, 3)); //Bru
+//Console.WriteLine( phrase.IndexOf("Brur") ); //Starts at [7]
+//Console.WriteLine(phrase[0]); // W
+//Console.WriteLine(phrase.Contains ("Wallah") ); <-true
+//Console.WriteLine(phrase.ToUpper());
+
+//Console.ReadLine();
+
+
+// MATH FUNCTIONS
+
+//int num = 6;
+//num++;
+//num--; 
+//Console.WriteLine(5 % 2); // 5 Divided by 2 and the Remainder is = 1
+
+//Console.WriteLine( Math.Pow(3, 2)); // 3 Raised to the second power = 9
+//Console.WriteLine(Math.Sqrt(36)); // SquareRoot
+//Console.WriteLine(Math.Max(50, 90)); // Tells what the biggest number is
+//Console.WriteLine(Math.Round(4.6)); // Rounds up the number to the closest whole number. In this case = 5
+
+//Console.ReadLine();
+
+
+// USER INPUT
+
+//Console.Write("Wallah, enter your name: ");
+//string name = Console.ReadLine(); // What the user writes will be stored in this variable called: name
+//Console.WriteLine("Wallah, enter your age: ");
+//string age = Console.ReadLine();
+//Console.WriteLine("Wallah " + name + ", you are " + age);
+
+//Console.ReadLine();
+
+
+
+// BUILDING A CALCULATOR
+
+//int num = Convert.ToInt32("56");
+//Console.WriteLine(num + 6);
+
+//Console.Write("Enter a number: ");
+//double num1 = Convert.ToDouble(Console.ReadLine());
+//Console.Write("Enter another number: ");
+//double num2 = Convert.ToDouble(Console.ReadLine());
+
+//Console.WriteLine(num1 + num2);
+
+//Console.ReadLine();
+
+
+
+//  MADLIBS
+
+//string color, pluralNoun, celebrity;
+
+//Console.Write("Enter a color: ");
+//color = Console.ReadLine();
+
+//Console.Write("Enter a plural noun: ");
+//pluralNoun = Console.ReadLine();
+
+//Console.Write("Enter a name of a celebrity: ");
+//celebrity = Console.ReadLine();
+
+//Console.WriteLine("Roses are " + color);
+//Console.WriteLine(pluralNoun + " are red");
+//Console.WriteLine("I love " + celebrity);
+
+
+
+
+// ARRAYS
+
+//int[] luckyNumbers = { 4, 5, 12, 53, 11, 45 };
+////luckyNumbers[1] = 90; // Kan endre på en variabel inni et array
+//string[] friends = new string[5]; // New string[] er størrelsen på array'et
+//friends[0] = "brur";
+//friends[1] = "Tjodleif";
+
+
+//Console.WriteLine(luckyNumbers[1]);
+
+//Console.ReadLine();
